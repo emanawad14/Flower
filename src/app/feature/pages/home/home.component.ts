bb00650d31e9edea13ca77acd18aca218cabfc47
emanawad14 <emanelnady2001@gmail.com>
2025-06-04 07:26:01 +0300

    addCustom

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { FeatureComponent } from "../feature/feature.component";
 import { NavComponent } from "../../../core/layout/nav/nav.component";
 import { FooterComponent } from "../../../core/layout/footer/footer.component";
 import { TrustedComponent } from "../trusted/trusted.component";
+import { CustomerComponent } from "../customer/customer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CategoryComponent, FeatureComponent, NavComponent, FooterComponent, TrustedComponent],
+  imports: [CategoryComponent, FeatureComponent, NavComponent, FooterComponent, TrustedComponent, CustomerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
